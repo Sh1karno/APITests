77f4205d38d4e7267e50843ecefc95bb51feeb56
p.surzhan <p.surzhan@omprussia.ru>
2021-01-29 17:50:26 +0300

    refactor base_module_test.py

--- base_module_test.py
+++ base_module_test.py
@@ -2,5 +2,5 @@ import requests
 
 
 def test_status_code_from_url(base_url, status_code):
-    response = requests.get(base_url, status_code)
+    response = requests.get(base_url)
     assert response.status_code == int(status_code)
